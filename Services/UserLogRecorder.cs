3287fceecfbbde3a037b5691d111166fc63204af
jaison.raju <jaison.raju@pncarchitects.com>
2026-03-13 09:13:47 +0400

    -new user feild data added

--- Services/UserLogRecorder.cs
+++ Services/UserLogRecorder.cs
@@ -1,5 +1,6 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
 using Autodesk.Revit.UI;
 using DocumentFormat.OpenXml.Spreadsheet;
 using PNCA_BIM_Suite_Library.Model;
@@ -12,40 +13,17 @@ namespace PNCA_BIM_Suite_Library.Services
 {
     internal static class UserLogRecorder
     {
-        public static async Task SendLog(string projectName, string addinName, string status, string message, string timeStart, string timeStop)
-        {
-            string url = "https://script.google.com/macros/s/AKfycbwlbiwtdfj_xsdvjjn5N1tS5UqZzvxheWxmMghQuk9VRL4Nwh3Uzj5Bq5hI4oAZa6mq/exec";
-
-            DateTime now = DateTime.Now;
-
-            var json = @"{
-            ""date"": """ + now.ToString("yyyy-MM-dd") + @""",
-            ""time"": """ + now.ToString("HH:mm:ss") + @""",
-            ""username"": """ + Environment.UserName + @""",
-            ""addin"": """ + addinName + @""",
-            ""project"": """ + projectName + @""",
-            ""timestart"": """ + now.ToString("HH:mm:ss") + @""",
-            ""timestop"": """ + now.ToString("HH:mm:ss") + @""",
-            ""status"": """ + status + @""",
-            ""message"": """ + message + @"""
-        }";
-
-            using (HttpClient client = new HttpClient())
-            {
-                var content = new StringContent(json, Encoding.UTF8, "application/json");
-                await client.PostAsync(url, content);
-            }
-        }
-
         public static async Task SendLog(UserLogData userLogData, Document doc)
         {
             // web api url
-            string url = "https://script.google.com/macros/s/AKfycbyS2i2hRFrl6BOKG4gjWYo2MshFiuWUlRiB7tfKlpAyT8aWmXf_u1LcpIYMH8EeWXoH/exec";
+            string url = "https://script.google.com/macros/s/AKfycbx_HHweKkC7PoOC3eOj5bOU30s90LalCVFNJ4nMajEjxxG91bmldP85q8n1j-nPUlyJ/exec";
 
             // data collection
             DateTime now = DateTime.Now;
             string projectName = doc.Title;
             string projectNumber = doc.ProjectInformation.Number;
+            string calculatedTimeSaving = "on test";
+
 
             var json = @"{
             ""date"": """ + now.ToString("yyyy-MM-dd") + @""",
@@ -58,7 +36,8 @@ namespace PNCA_BIM_Suite_Library.Services
             ""status"": """ + userLogData.Status + @""",
             ""message"": """ + userLogData.Message + @""",
             ""fullusername"": """ + Environment.UserDomainName + @""",
-            ""projectnumber"": """ + projectNumber + @"""
+            ""projectnumber"": """ + projectNumber + @""",
+            ""calculatedtimesaving"": """ + calculatedTimeSaving + @"""
 
         }";
 
@@ -68,8 +47,5 @@ namespace PNCA_BIM_Suite_Library.Services
                 await client.PostAsync(url, content);
             }
         }
-
-
     }
-
 }
